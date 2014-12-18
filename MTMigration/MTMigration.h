35588a89781f52fb4dfaf1e9ba0128e23affc457
Viktor Fröberg <vik.froberg@gmail.com>
2014-12-18 11:12:42 +0100

    Migrate to build number

--- MTMigration/MTMigration.h
+++ MTMigration/MTMigration.h
@@ -14,22 +14,32 @@ typedef void (^MTExecutionBlock)(void);
 
 @interface MTMigration : NSObject
 
-/** 
+/**
  Executes a block of code for a specific version number and remembers this version as the latest migration done by MTMigration.
- 
+
  @param version A string with a specific version number
  @param migrationBlock A block object to be executed when the application version matches the string 'version'. This parameter can't be nil.
- 
+
  */
 
 + (void) migrateToVersion:(NSString *)version block:(MTExecutionBlock)migrationBlock;
 
-/** 
- 
+/**
+ Executes a block of code for a specific build number and remembers this build as the latest migration done by MTMigration.
+
+ @param buildNumber A string with a specific build number
+ @param migrationBlock A block object to be executed when the application build matches the string 'build'. This parameter can't be nil.
+
+ */
+
++ (void) migrateToBuild:(NSString *)build block:(MTExecutionBlock)migrationBlock;
+
+/**
+
  Executes a block of code for every time the application version changes.
- 
+
  @param updateBlock A block object to be executed when the application version changes. This parameter can't be nil.
- 
+
  */
 
 + (void) applicationUpdateBlock:(MTExecutionBlock)updateBlock;
