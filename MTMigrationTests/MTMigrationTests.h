9b86e3d9cb6ebc17799f569479ef7a49ada516af
alasdair <alasdair.mccall@musicqubed.com>
2015-04-15 23:33:04 +0100

    - Updated SenTests to XCTests. - Used `XCTestExpectations` feature to better check whether blocks are called. - Removed `__block NSInteger val = 0;` used to test blocks.

--- MTMigrationTests/MTMigrationTests.h
+++ MTMigrationTests/MTMigrationTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 Mysterious Trousers. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface MTMigrationTests : SenTestCase
+@interface MTMigrationTests : XCTestCase
 
 @end
