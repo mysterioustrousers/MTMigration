4e09788f8f779a15d364cca8bf008af45c883cc1
Herman Olsson <herman.olsson@gmail.com>
2015-10-27 15:28:50 +0100

    Remove script and use xcode 7.1.

--- scripts/tests.sh
+++
@@ -1,2 +0,0 @@
-xctool -project MTMigration.xcodeproj -scheme MTMigration -sdk iphonesimulator9.1 test
-xctool -project ./Swift/MTMigration.xcodeproj -scheme MTMigration -sdk iphonesimulator9.1 test
